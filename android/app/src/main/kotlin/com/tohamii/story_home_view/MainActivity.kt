e3f61ae47044058df95601665a6642a19e511f45
kariim8 <KariimAhmed8@gmail.com>
2019-10-30 23:53:09 +0200

    initial

---
+++ android/app/src/main/kotlin/com/tohamii/story_home_view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tohamii.story_home_view
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
